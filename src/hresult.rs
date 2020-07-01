95fc1affabb0e362dc089288bd6df5a6a46a5746
Jari Pennanen <ciantic@oksidi.com>
2020-07-01 23:04:28 +0300

    docs etc...

--- src/hresult.rs
+++ src/hresult.rs
@@ -1,21 +1,33 @@
 use std::fmt::Debug;
 
+/// This is wrapper for handling HRESULT values.
+///
+/// Value is printed in hexadecimal format for convinience, this is usually the
+/// format it's given in MSDN.
 #[derive(PartialEq, PartialOrd, Clone, Copy)]
 pub struct HRESULT(i32);
 
 impl HRESULT {
+    /// Is any failure?
     #[inline]
     pub fn failed(&self) -> bool {
-        self < &HRESULT::from_i32(0)
+        self.0 < 0
     }
+
+    /// Test if the HRESULT failed with certain value, e.g.
+    /// hresult.failed_with(0x800706BA)
     #[inline]
     pub fn failed_with(&self, u: u32) -> bool {
         self.0 == u as i32
     }
+
+    /// Indicates not a failure
     #[inline]
     pub fn ok() -> HRESULT {
         HRESULT(0)
     }
+
+    /// Create value
     #[inline]
     pub fn from_i32(v: i32) -> HRESULT {
         HRESULT(v)
